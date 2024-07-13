6b7728bc52c610bbaf37b8f2c5a41c01da4619f5
gsmSahani <sahanigautam0446162@gmail.com>
2024-07-13 15:30:55 +0530

    sum of given array using different method

---
+++ Array/sumOfAllNumber.js
@@ -0,0 +1,23 @@
+// calculate the sum of allnumber in  an array
+
+// Approach 1 using for loop
+// const number = [1, 2, 3, 4, 5, 6];
+// let sum = 0;
+// for (let i = 0; i < number.length; i++) {
+//   sum = sum + number[i];
+// }
+// console.log(sum);
+
+// Approach 2 reduce()
+// const number = [1, 2, 3, 4, 5, 6];
+// const sumOfNumber = number.reduce((acc, current) => acc + current, 0);
+// console.log(sumOfNumber);
+
+// Approach 3 forEach ()
+
+// const number = [1, 2, 3, 4, 5, 6];
+// let sumOfNumber = 0;
+// number.forEach((numbers) => {
+//     sumOfNumber += numbers;
+// });
+// console.log(sumOfNumber);
