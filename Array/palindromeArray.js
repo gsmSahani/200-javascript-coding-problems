d73d510b6c5faeeb84499eeb67d1f237a92bda7f
gsmSahani <sahanigautam0446162@gmail.com>
2024-07-15 15:21:36 +0530

    checking arry as palindrome or not

---
+++ Array/palindromeArray.js
@@ -0,0 +1,12 @@
+// function isArrayPalindrom(arr) {
+//   let reversedArrAsString = arr.slice().reverse();
+//   return JSON.stringify(arr) === JSON.stringify(reversedArrAsString);
+// }
+// const arr1 = [1, 2, 3, 2, 1];
+// const arr2 = [7, 8, 9, 4, 5, 6];
+// console.log(isArrayPalindrom(arr1));
+// console.log(isArrayPalindrom(arr2));
+
+// const obj = { name: "Gautam", age: 30, city: "Valsad" };
+// const myJSON = JSON.stringify(obj);
+// console.log(myJSON);
