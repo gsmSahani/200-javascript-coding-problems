10b20807ba99a09e4b5993566cb42dadc1b85031
gsmSahani <sahanigautam0446162@gmail.com>
2024-07-22 15:50:45 +0530

    basic loop questions

---
+++ Basics/PrintNum1-15.js
@@ -0,0 +1,18 @@
+// print the number from 1 to 15
+// let i = 1;
+// for (let i = 1; i <= 15; i++) {
+//   console.log(i);
+// }
+// while looop
+// a = 0;
+// while (a < 15) {
+//   a++;
+//   console.log(a);
+// }
+
+// do while
+b = 0;
+do {
+    b++;
+    console.log(b);
+} while (b <= 15);
