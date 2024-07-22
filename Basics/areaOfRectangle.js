357f76c8130eaa2639a70bc79706e1a869e0b0c1
gsmSahani <sahanigautam0446162@gmail.com>
2024-07-22 16:07:10 +0530

    area of rectangel with template literals

---
+++ Basics/areaOfRectangle.js
@@ -0,0 +1,6 @@
+// write a function to calculate the area of reactangle given width and height
+// formul a=w*l
+function calculateArea(w, l) {
+  return w * l;
+}
+console.log(`Area of rectangle is ${calculateArea(10, 20)}`);
