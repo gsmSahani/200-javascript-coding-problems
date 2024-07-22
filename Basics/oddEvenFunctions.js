25ad23d57aa20818cefeb080fb2cfecbc602d044
gsmSahani <sahanigautam0446162@gmail.com>
2024-07-22 16:33:49 +0530

    function to check if the given number is odd or even

---
+++ Basics/oddEvenFunctions.js
@@ -0,0 +1,9 @@
+// write a function to check if the number is odd or even
+function oddEven(num) {
+  if (num % 2 === 0) {
+    return "even";
+  } else {
+    return "odd";
+  }
+}
+console.log(oddEven(101));
