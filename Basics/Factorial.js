84c4a1958149ea3e720369be2327a72e52f5ea5c
gsmSahani <sahanigautam0446162@gmail.com>
2024-07-26 10:19:45 +0530

    factorial with both recursive and traditional method

--- Basics/Factorial.js
+++ Basics/Factorial.js
@@ -1,14 +1,25 @@
 // write a function that calculates the factorial of given number
 // factorial means 5! = 5*4*3*2*1=120
-function factorial(num) {
-  if (num === 0 || num === 1) {
+// function factorial(num) {
+//   if (num === 0 || num === 1) {
+//     return 1;
+//   } else {
+//     let fact = 1;
+//     for (let i = 1; i <= num; i++) {
+//       fact = fact * i;
+//     }
+//     return fact;
+//   }
+// }
+// console.log(factorial(5));
+
+// modern method
+function factorial(number) {
+  if (number === 0 || number === 1) {
     return 1;
   } else {
-    let fact = 1;
-    for (let i = 1; i <= num; i++) {
-      fact = fact * i;
-    }
-    return fact;
+    return number * factorial(number - 1);
   }
 }
-console.log(factorial(5));
+let result = factorial(5);
+console.log(result);
