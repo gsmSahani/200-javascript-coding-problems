74256c5bb5d0526243acc3eb8c7221258b7ca770
gsmSahani <sahanigautam0446162@gmail.com>
2024-07-27 17:21:26 +0530

    checking if the object is an array or not

---
+++ Object/checkObject.js
@@ -0,0 +1,5 @@
+// check if the object is an array or not
+let obj = [{ name: "gautam" },{company:'Shamira Infotech'}];
+let checkArr = Array.isArray(obj);
+console.log(checkArr);
+console.log(obj[0].name)
