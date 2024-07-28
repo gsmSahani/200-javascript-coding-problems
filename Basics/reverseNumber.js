eced34f7abd26fa1ad3231d49968784714d09097
gsmSahani <sahanigautam0446162@gmail.com>
2024-07-28 19:12:48 +0530

    added another method

--- Basics/reverseNumber.js
+++ Basics/reverseNumber.js
@@ -3,6 +3,18 @@ function reverseNumber() {
   let a = 7890;
   let splitted = Number(a.toString().split("").reverse().join(""));
   console.log(splitted);
-  console.log(isNaN(splitted));//we are checking if the splitted is number or not
+  console.log(isNaN(splitted)); //we are checking if the splitted is number or not
 }
 reverseNumber();
+
+// traditional method
+function revNumber(num) {
+  let rev = 0;
+  while (num > 0) {
+    let rem = num % 10;
+    rev = rev * 10 + rem;
+    num = Math.floor(num / 10);
+  }
+  return rev;
+}
+console.log(revNumber(12345));
