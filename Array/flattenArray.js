bcb40171a2dbad3f13c3c8a14cf71c7d3d204e88
gsmSahani <sahanigautam0446162@gmail.com>
2024-08-19 19:16:03 +0530

    flattened the nested array with various method

--- Array/flattenArray.js
+++ Array/flattenArray.js
@@ -1,30 +1,46 @@
-// write a js program to flatten the array
-
-const numbers = [1, 2, 3, 4, 5, 6, [7, 8, 9, [3, 4, 5], 2, 3, 4], 7, 8, 9];
-// console.log(numbers.flat()); //shallow copy flatten the first 1 or (nested) array inside the main array
-// console.log(numbers.flat(1));
-const customFlat = (numbers, depth = 1) => {
+// Approach 1:foreach method
+// function flattenArray(arr) {
+//   let flattened = [];
+//   arr.forEach((element) => {
+//     if (Array.isArray(element)) {
+//       flattened.push(...flattenArray(element));
+//     } else {
+//       flattened.push(element);
+//     }
+//   });
+//   return flattened;
+// }
+// const nestedArray = [1, 2, 3, 4, 5, 6, [7, 8, 9, [3, 4, 5], 2, 3, 4], 7, 8, 9];;
+// console.log("Flattened Array:", flattenArray(nestedArray));
+// -----------------------------------------------------------------
+// Approach 2: Array.prototype.reduce and concat
+// function flattenArray(arr) {
+//   return arr.reduce((flat, toFlatten) => {
+//     return flat.concat(
+//       Array.isArray(toFlatten) ? flattenArray(toFlatten) : toFlatten
+//     );
+//   },[]);
+// }
+// const nestedArray = [1, 2, 3, 4, 5, 6, [7, 8, 9, [3, 4, 5], 2, 3, 4], 7, 8, 9];;
+// console.log("Flattened Array:", flattenArray(nestedArray));
+// ------------------------------------------------------------------
+// Approach 3: array.prototype.flat es6+
+// const nestedArray = [1, 2, 3, 4, 5, 6, [7, 8, 9, [3, 4, 5], 2, 3, 4], 7, 8, 9];
+// const flattenedArray = nestedArray.flat(Infinity);
+// console.log("Flatten Array:", flattenedArray);
+// ---------------------------------------------------------------------
+const customFlat = (nestedArray, depth = 1) => {
   const result = [];
-  for (let i = 0; i <= numbers.length - 1; i++) {
-    if (Array.isArray(numbers[i]) && depth > 1) {
-      result.push(...customFlat(numbers[i], depth - 1));
+  for (let i = 0; i <= nestedArray.length - 1; i++) {
+    if (Array.isArray(nestedArray[i]) && depth > 1) {
+      result.push(...customFlat(nestedArray[i], depth - 1));
     } else {
-      result.push(numbers[i]);
+      result.push(nestedArray[i]);
     }
   }
   return result;
 };
-console.log(customFlat(numbers, 1)); //flatten the 1st level array
-console.log(customFlat(numbers, 2)); //flatten the 2nd level array
-console.log(customFlat(numbers, Infinity)); //it will completely flatten the array
-/*
-[ 1, 2, 3, 4, 5, 6, [ 7, 8, 9, [ 3, 4, 5 ], 2, 3, 4 ], 7, 8, 9 ]
-[ 1, 2, 3, 4, 5, 6, 7, 8, 9, [ 3, 4, 5 ], 2, 3, 4, 7, 8, 9 ]
-[
-  1, 2, 3, 4, 5, 6, 7,
-  8, 9, 3, 4, 5, 2, 3,
-  4, 7, 8, 9
-]
-
-
-*/
+const nestedArray = [1, 2, 3, 4, 5, 6, [7, 8, 9, [3, 4, 5], 2, 3, 4], 7, 8, 9];
+console.log(customFlat(nestedArray, 1)); //flatten the 1st level array
+console.log(customFlat(nestedArray, 2)); //flatten the 2nd level array
+console.log(customFlat(nestedArray, Infinity)); //it will completely flatten the array
