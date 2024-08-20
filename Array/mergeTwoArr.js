29d9771c16786ac0f73cfa39e887cf3c82285470
gsmSahani <sahanigautam0446162@gmail.com>
2024-08-20 14:33:51 +0530

    merging two array

---
+++ Array/mergeTwoArr.js
@@ -0,0 +1,8 @@
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [9, 8, 7, 6, 5];
+const result = [...arr1, ...arr2];
+const result2 = arr1.concat(arr2)
+console.log(result2)
+// console.log(result);
+
+// using 2 approach spread and concat 
